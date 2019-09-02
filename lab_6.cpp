077b1a56973c725ae29a221dbbdb65ca73663258
Krishna Annapurne <44083052+krishnaannapurne@users.noreply.github.com>
2019-09-02 22:22:05 +0530

    Update lab_6.cpp

--- lab_6.cpp
+++ lab_6.cpp
@@ -1,30 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
 
-int main(){
 
-	int no_of_users, no_of_op, op_no, max=0, max_user=-1, user_id, amount;
-	int A[no_of_op];
-	cin>>no_of_users;
-	cin>>no_of_op;
-
-	for(int i=0; i<no_of_users; i++)
-	A[i]=0;
-
-	while(no_of_op--){
-		cin>>op_no;
-		if(op_no==2){
-			if(max_user==-1) cout<<"No data available\n";
-			else cout<<max_user<<endl;
-		}else{
-			//operation 1 
-
-			cin>>user_id;
-			cin>>amount;
-
-			A[user_id]+= amount;
-
-			if(A[user_id]>max) max_user=user_id;
-		}
-	}
+int main()
+{
+    pair<ll, ll> p;
+    p = {-1, -1};
+    ll no_of_queries;
+    ll no_of_users;
+    cin >> no_of_users >> no_of_queries;
+    vector<pair<ll, ll>> v;
+    int x = 0;
+    while (no_of_queries--)
+    {
+        cin >> x;
+        if (x == 1)
+        {
+            ll userid, amount, i;
+            cin >> userid >> amount;
+            if (v.empty())
+            {
+                v.push_back({userid, amount});
+            }
+            else
+            {
+                for (i = 0; i < v.size(); i++)
+                {
+                    if (userid == v[i].first)
+                    {
+                        v[i].second += amount;
+                        amount=v[i].second;
+                        break;
+                    }
+                }
+                if (i == v.size())
+                {
+                    v.push_back({userid,amount});
+                }
+            }
+            if (amount > p.second)
+            {
+                p = {userid,amount};
+            }
+        }
+        else if (x == 2)
+        {
+            if (p.first == -1)
+            {
+                cout << "No data available\n";
+            }
+            else
+            {
+                cout << p.first << "\n";
+            }
+        }
+        else cout<<"Invalid input\n";
+    }
 }
